18423f0275c9a764a0dc32053b0fee84a169b102
Jome Favourite <jfjomefavourite@gmail.com>
2025-03-21 00:31:19 +0100

    refactor: remove unused iconWithClassName utility function from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,20 +7,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-import { cssInterop } from 'nativewind';
-
-export function iconWithClassName(icon: any) {
-  cssInterop(icon, {
-    className: {
-      target: 'style',
-      nativeStyleToProp: {
-        color: true,
-        opacity: true,
-      },
-    },
-  });
-}
-
 export interface OnboardingStep {
   title: string;
   subtile: string;
