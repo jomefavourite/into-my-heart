116f58527532d6377baab23715254488111841f5
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-03-31 23:16:49 +0100

    feat: refactor VersesHomeScreen to use Tabs component; remove old button navigation and .env file

--- app/(tabs)/verse/index.tsx
+++ app/(tabs)/verse/index.tsx
@@ -1,46 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
 import Container from '~/components/Container';
-import VersesTab from './versesTab';
-import { Animated, StyleSheet, View } from 'react-native';
 import Title from '~/components/Title';
-import CustomButton from '~/components/CustomButton';
+import ThemedText from '~/components/ThemedText';
+import VersesTab from './versesTab';
 import CollectionsTab from './CollectionsTab';
-import { useTabStore } from '~/store/tab-store';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
+import { cn } from '~/lib/utils';
+import { Animated } from 'react-native';
+import AddIcon from '~/assets/icons/AddIcon';
+import GridViewIcon from '~/assets/icons/GridViewIcon';
 
 export default function VersesHomeScreen() {
-  const { activeTab, setActiveTab } = useTabStore();
+  const [value, setValue] = useState('verses');
+
   return (
     <Container>
-      <View>
-      <Title text='Verses & Collections' />
-      <View className='flex flex-row gap-3 py-3'>
-          <CustomButton 
-            className={activeTab === 'verses' ? 'w-fit' : 'bg-white w-fit'} 
-            textClassName={activeTab === 'verses' ? 'text-white' : 'text-gray-600'}
-            onPress={() => setActiveTab('verses')}
-          >
-            Verses
-          </CustomButton>
-          <CustomButton 
-            className={activeTab === 'collections' ? 'w-fit' : 'bg-white w-fit'}
-            textClassName={activeTab === 'collections' ? 'text-white' : 'text-gray-600'}  
-            onPress={() => setActiveTab('collections')}
-          >
-            Collections
-          </CustomButton>
-        </View>
+      <View className="gap-5">
+      <ThemedText size={22} variant='semibold'>
+          Verses & Collections
+      </ThemedText>
 
-        <Animated.View style={{ opacity: activeTab === 'verses' ? 1 : 0 }}>
-          {activeTab === 'verses' && <VersesTab />}
-        </Animated.View>
-        <Animated.View style={{ opacity: activeTab === 'collections' ? 1 : 0 }}>
-          {activeTab === 'collections' && <CollectionsTab />}
-        </Animated.View>
-    </View>
+        <Tabs
+          value={value}
+          onValueChange={setValue}
+          className="w-full mx-auto flex-col gap-1.5"
+        >
+          <View className='relative flex justify-center'>
+          <TabsList className="flex-row">
+            <TabsTrigger value="verses" className="">
+              <ThemedText
+                size={13}
+                variant="medium"
+                className={cn(
+                  'text-muted-foreground',
+                  value === 'verses' && 'text-white dark:text-primary-foreground'
+                )}
+              >
+                Verses
+              </ThemedText>
+            </TabsTrigger>
+            <TabsTrigger value="collections" className="w-fit">
+              <ThemedText
+                size={13}
+                variant="medium"
+                className={cn(
+                  'text-muted-foreground',
+                  value === 'collections' && 'text-white dark:text-primary-foreground'
+                )}
+              >
+                Collections
+              </ThemedText>
+            </TabsTrigger>
+          </TabsList>
+          <View className='absolute right-0 flex items-center px-2'>
+            <AddIcon />
+            <GridViewIcon />
+          </View>
+          </View>
+
+          <Animated.View style={{ opacity: value === 'verses' ? 1 : 0 }}>
+            <TabsContent value="verses">
+              <VersesTab />
+            </TabsContent>
+          </Animated.View>
+          
+          <Animated.View style={{ opacity: value === 'collections' ? 1 : 0 }}>
+            <TabsContent value="collections">
+              <CollectionsTab />
+            </TabsContent>
+          </Animated.View>
+        </Tabs>
+      </View>
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
