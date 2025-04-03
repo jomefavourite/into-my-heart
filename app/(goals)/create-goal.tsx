1b5d3705d37ad78a638a01a3968abc800ff672ca
Jome Favourite <jfjomefavourite@gmail.com>
2025-04-03 23:16:55 +0100

    feat: wrap CreateGoal component in SafeAreaView for improved layout handling

--- app/(goals)/create-goal.tsx
+++ app/(goals)/create-goal.tsx
@@ -11,6 +11,7 @@ import UnfoldMoreIcon from '~/assets/icons/UnfoldMoreIcon';
 import { useColorScheme } from '~/hooks/useColorScheme';
 import ArrowLeftIcon from '~/assets/icons/ArrowLeftIcon';
 import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function CreateGoal() {
   const { colorScheme } = useColorScheme();
@@ -28,77 +29,80 @@ export default function CreateGoal() {
   const reviewFreqValue = useBottomSheetStore((state) => state.reviewFreqValue);
 
   return (
-    <Container>
-      <View className='items-center justify-between flex-row mb-7'>
-        <Button size={'icon'} variant={'ghost'} onPress={() => router.back()}>
-          <ArrowLeftIcon />
-        </Button>
-
-        {/* This should be centered properly, it's not currently */}
-        <ThemedText size={16} variant='medium'>
-          Create Goal
-        </ThemedText>
-
-        <View></View>
-      </View>
-
+    <SafeAreaView>
       <View className='flex-1'>
-        <View className='gap-1 pb-3'>
-          <Label nativeID='goalName'>Goal Name</Label>
-          <Input
-            aria-aria-labelledby='goalName'
-            defaultValue=''
-            placeholder='Enter goal name'
-          />
-        </View>
-
-        <View className='border-1 border-[#E8E8E8] dark:border-[#E8E8E8] py-4 border-y flex-row items-center justify-between'>
-          <ThemedText size={14} variant='medium'>
-            Verses
-          </ThemedText>
-          <Button
-            size={'icon'}
-            className='bg-transparent'
-            // onPress={() => router.push('/(tabs)/goals/create-goal')}
-          >
-            <AddIcon stroke='white' />
+        <View className='items-center justify-between flex-row mb-7 p-[18px]'>
+          <Button size={'icon'} variant={'ghost'} onPress={() => router.back()}>
+            <ArrowLeftIcon />
           </Button>
-        </View>
 
-        <View className='border-b border-[#E8E8E8] dark:border-[#E8E8E8] py-4 flex-row gap-5'>
-          <View className='flex-1'>
-            <ThemedText size={14} variant='medium'>
-              Start date
-            </ThemedText>
-            <Button onPress={() => setStartDateBottomSheetIndex(1)}>
-              <ThemedText>hello</ThemedText>
-            </Button>
-          </View>
-          <View className='flex-1'>
-            <ThemedText size={14} variant='medium'>
-              End date
-            </ThemedText>
-            <Button onPress={() => setStartDateBottomSheetIndex(1)}>
-              <ThemedText>hello</ThemedText>
-            </Button>
-          </View>
-        </View>
-
-        <View className='border-b border-[#E8E8E8] flex-row items-center justify-between dark:border-[#E8E8E8] py-4'>
-          <ThemedText size={14} variant='medium'>
-            Review Frequency
+          {/* This should be centered properly, it's not currently */}
+          <ThemedText size={16} variant='medium'>
+            Create Goal
           </ThemedText>
 
-          <Button
-            variant={'ghost'}
-            className='w-fit flex-row gap-2 items-center'
-            onPress={() => setReviewFreqIndex(1)}
-          >
-            <ThemedText>{reviewFreqValue}</ThemedText>
-            <UnfoldMoreIcon />
-          </Button>
+          <View></View>
+        </View>
+
+        <View className='flex-1'>
+          <View className='gap-1 pb-3'>
+            <Label nativeID='goalName'>Goal Name</Label>
+            <Input
+              aria-aria-labelledby='goalName'
+              defaultValue=''
+              placeholder='Enter goal name'
+              className='dark:text-white'
+            />
+          </View>
+
+          <View className='border-1 border-[#E8E8E8] dark:border-[#E8E8E8] py-4 border-y flex-row items-center justify-between'>
+            <ThemedText size={14} variant='medium'>
+              Verses
+            </ThemedText>
+            <Button
+              size={'icon'}
+              className='bg-transparent'
+              // onPress={() => router.push('/(tabs)/goals/create-goal')}
+            >
+              <AddIcon stroke='white' />
+            </Button>
+          </View>
+
+          <View className='border-b border-[#E8E8E8] dark:border-[#E8E8E8] py-4 flex-row gap-5'>
+            <View className='flex-1'>
+              <ThemedText size={14} variant='medium'>
+                Start date
+              </ThemedText>
+              <Button onPress={() => setStartDateBottomSheetIndex(1)}>
+                <ThemedText>hello</ThemedText>
+              </Button>
+            </View>
+            <View className='flex-1'>
+              <ThemedText size={14} variant='medium'>
+                End date
+              </ThemedText>
+              <Button onPress={() => setStartDateBottomSheetIndex(1)}>
+                <ThemedText>hello</ThemedText>
+              </Button>
+            </View>
+          </View>
+
+          <View className='border-b border-[#E8E8E8] flex-row items-center justify-between dark:border-[#E8E8E8] py-4'>
+            <ThemedText size={14} variant='medium'>
+              Review Frequency
+            </ThemedText>
+
+            <Button
+              variant={'ghost'}
+              className='w-fit flex-row gap-2 items-center'
+              onPress={() => setReviewFreqIndex(1)}
+            >
+              <ThemedText>{reviewFreqValue}</ThemedText>
+              <UnfoldMoreIcon />
+            </Button>
+          </View>
         </View>
       </View>
-    </Container>
+    </SafeAreaView>
   );
 }
